2eb6a7d7e1b428bf745a344102e778dc1bc5119c
kirkozlov <kirill.kozlov.fhaachen@gmail.com>
2015-12-09 10:44:53 +0100

    send mail gemacht

---
+++ includes/sendmail.php
@@ -0,0 +1,30 @@
+<?php
+require_once "Mail.php";
+
+$from = '<fhswege3@gmail.com>';
+$to = '<kirill.kozlov.fhaachen@gmail.com>';
+$subject = 'Hi!';
+$body = "Hi,\n\nHow are you?";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'fhswege3@gmail.com',
+        'password' => '23146758'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
+?>
