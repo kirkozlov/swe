704ed6b9bda775686ae89a29b34db4b4ff60470d
kirkozlov <kirill.kozlov.fhaachen@gmail.com>
2016-01-06 11:16:47 +0100

    PopUp mit 2 Button

--- meineanzeigen.php
+++ meineanzeigen.php
@@ -48,7 +48,24 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
         <link rel="stylesheet" href="css/main.css" type="text/css" />
         <link rel="stylesheet" href="css/menu.css" type="text/css" />
-
+		<script src="includes/jquery.js"></script>
+		<script >
+		$(document).ready(function(){
+			    PopUpHide();
+		});
+		function PopUpShow(){
+			$("#popup").show();
+		}
+		function DeleteOnClick(value2){
+			document.getElementById('jaB').value=value2;
+			PopUpShow();
+		}
+		function PopUpHide(){
+			$("#popup").hide();
+			
+		}
+		
+		</script>
     </head>
     <body>
         <?php 
@@ -73,13 +90,13 @@
 								echo '<td>'.$value.'</td>';
 								echo '<td>
 										<form id="edit" action="edititem.php" method="post" enctype="multipart/form-data" >
-											<button value="'.$interid[$key].'" name="edit"/>Editieren</button>
+											<button value="'.$interid[$key].'" name="edit">Editieren</button>
 										</form>
 									</td>';
 								echo '<td>
-										<form id="delete" action="" method="post" enctype="multipart/form-data" >
-											<button value="'.$interid[$key].'" name="delete"/>Löschen</button>
-										</form>
+										
+											<button  onclick="DeleteOnClick('.$interid[$key].')">Löschen</button>
+										
 									</td>';
 								echo '</tr>';
 							}
@@ -88,6 +105,32 @@
 				</table>
             </div>
         </div>
+		
+		<div id="popup"  style=" width:100%;
+												height: 2000px;
+												background-color: rgba(0,0,0,0.5);
+												overflow:hidden;
+												position:fixed;
+												top:0px;">
+			<div id="ppc" style="margin:40px auto 0px auto;
+												width:250px;
+												height: 40px;
+												padding:10px;
+												
+												background-color: #c5c5c5;
+												border-radius:5px;
+												box-shadow: 0px 0px 10px #000;">
+				<div id="ppt" style="align:center;" >
+					<p id="BestP">Sind Sie sicher?</p>
+					<form id="delete" action="" method="post" enctype="multipart/form-data" >
+						<button id="jaB" value="" name="delete">Ja</button>
+						<button id="neinB" onclick="PopUpHide(); return false;">Nein</button>
+					</form>
+					
+				</div>
+			
+			</div>
+		</div>
     </body>
 </html>
 
